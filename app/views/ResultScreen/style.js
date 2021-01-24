42e00296d9eaa42fab0058ac0e2330308ae76ec0
Ahmet <contact@ahmetcabak.com>
2021-01-24 17:01:34 +0100

    result

--- app/views/ResultScreen/style.js
+++ app/views/ResultScreen/style.js
@@ -10,24 +10,28 @@ const styles = StyleSheet.create({
     capture: {
         backgroundColor: "#3D3D65",
         // height: '80%',
-        flex:1,
+        paddingBottom: 50,
         marginBottom: 20,
         width: '90%',
         borderRadius: 10,
-        marginTop: '5%',
+        marginTop: '10%',
         alignItems: 'center',
+        overflow: 'visible',
+        paddingTop: 20
     },
     logo: {
         height: Dimensions.get('screen').height * 0.1,
         width: Dimensions.get('screen').height * 0.1,
-        marginTop: 10
+        
+        alignSelf: 'center'
     },
     messageContainer: {
         backgroundColor: '#1B1B2F',
         width: '90%',
         marginTop: 20,
         padding: 15,
-        borderRadius: 10
+        borderRadius: 10,
+        
     },
     messageText: {
         color: "#FFF",
@@ -71,6 +75,13 @@ const styles = StyleSheet.create({
         backgroundColor: '#3D3D65',
         padding: 20,
         borderRadius: 10
+    },
+    swipeupText: {
+        color: '#FFF',
+        fontFamily: 'SF Compact Rounded',
+         fontWeight: '800',
+         fontSize: 20,
+         alignSelf: 'center',
     }
 })
 
