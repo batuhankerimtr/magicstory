2a8d7bd6de497394fa3c3269088eda7d6b2eb538
Ahmet <contact@ahmetcabak.com>
2021-01-26 21:08:28 +0100

    ios release

--- app/views/LandingScreen/index.js
+++ app/views/LandingScreen/index.js
@@ -32,7 +32,7 @@ const SplashScreen = (props) => {
       <Pressable onPress={() => nextBtn()} style={styles.continueBtn}>
         <Text style={styles.continueBtnText}>Continuer</Text>
       </Pressable>
-      <Text style={styles.acceptText}>En continuant vous acceptez les Conditions Générales & Privacy de Magic Story.</Text>
+      <Text style={styles.acceptText}>En continuant svous acceptez les Conditions Générales & Privacy de Magic Story.</Text>
       <View style={styles.linkGroup}>
         <Pressable onPress={() => Linking.openURL("https://magicstory.vercel.app/terms")} style={styles.link}><Text style={styles.linkText}>C.G.U</Text></Pressable>
         <Pressable onPress={() => Linking.openURL("https://magicstory.vercel.app/privacy")} style={styles.link}><Text style={styles.linkText}>Privacy</Text></Pressable>
