fd5a900fd25f6b4a63d1b05b66f084d7242f06d4
Batuhan Kerim Türk <batuhankerimtr@gmail.com>
2021-01-26 13:03:18 +0300

    info and title

--- app/views/MainScreen/index.js
+++ app/views/MainScreen/index.js
@@ -9,7 +9,7 @@ import {
 import styles from './style'
 import ReactNativeHapticFeedback from "react-native-haptic-feedback";
 import * as Animatable from 'react-native-animatable';
-import RNStoryShare from "react-native-story-share";
+import Icon from 'react-native-vector-icons/dist/FontAwesome5';
 import axios from "axios";
 const options = {
   enableVibrateFallback: true,
@@ -31,7 +31,7 @@ const SplashScreen = (props) => {
   const magitize = async() => {
     handleViewRef.current.pulse(300)
     ReactNativeHapticFeedback.trigger("impactHeavy", options);
-    await axios.get('https://magicstory-web.vercel.app/getMagic')
+    await axios.get('https://magicstory.vercel.app/getMagic')
     .then(function (response) {
       props.navigation.navigate('Result', {message: response.data.message})
       handleViewRef.current.pulse(700)
@@ -42,9 +42,16 @@ const SplashScreen = (props) => {
   }
   return (
     <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Magic Story</Text>
       <Pressable onPress={() => magitize()}>
         <Animatable.Image ref={handleViewRef} duration={count} animation="pulse" easing="ease-out" iterationCount="infinite" style={styles.logo} source={require('../../../assets/img/icon.png')} />
       </Pressable>
+      <View style={styles.infoLabel}>
+        <View style={styles.infoIcon}>
+          <Icon name="exclamation" color="#FFFC00"/>
+        </View>
+        <Text style={styles.infoText}>Appuyez sur la boule doucement</Text>
+      </View>
     </SafeAreaView>
   );
 };
--- app/views/MainScreen/style.js
+++ app/views/MainScreen/style.js
@@ -5,12 +5,37 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#1B1B2F',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+    },
+    title: {
+        color: '#FFF',
+        fontSize:24,
+        fontFamily:  Platform.OS == "ios" ? 'SF Compact Rounded': 'SF-Compact-Rounded-Bold',
+        fontWeight: '700',
     },
     logo: {
         height: Dimensions.get('window').height * 0.25,
-        width: Dimensions.get('window').height * 0.25
+        width: Dimensions.get('window').height * 0.25,
+        marginVertical:150
     },
+    infoLabel: {
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        backgroundColor:"#3D3D65", 
+        borderRadius:100, 
+        paddingHorizontal:10, 
+        paddingVertical:3
+    },
+    infoIcon: {
+        height:10, 
+        width:10
+    },
+    infoText: {
+        color:'white', 
+        fontSize:12, 
+        fontFamily:  Platform.OS == "ios" ? 'SF Compact Rounded': 'SF-Compact-Rounded-Light',
+        fontWeight:"300"
+    }
 })
 
 export default styles;
